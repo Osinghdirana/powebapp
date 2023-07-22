5ddd8a06f13cfb77fd990ec897650f3c746e5051
Digamber Singh <digamber.singh@verint.com>
2023-07-22 20:28:59 +0530

    Initial Commit

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PurchaseOrderExtraction.Models;
+using PurchaseOrderExtraction.Services;
 
 namespace powebapp.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public List<Product> Products;
+
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            Products = new List<Product>();
         }
 
         public void OnGet()
         {
-
+            ProductService productService = new ProductService();
+            Products = productService.GetProducts();
         }
     }
 }
---
+++ Services/ProductService.cs
@@ -0,0 +1,59 @@
+﻿using PurchaseOrderExtraction.Models;
+using System.Data.SqlClient;
+
+namespace PurchaseOrderExtraction.Services
+{
+    public class ProductService
+    {
+        private static string db_source = "poserver1.database.windows.net";
+        private static string db_name = "podb";
+        private static string db_userid = "sqlAdmin";
+        private static string db_password = "Mitsui@123";
+
+        //Get Connection
+        private SqlConnection GetConnection()
+        {
+           var _builder = new SqlConnectionStringBuilder();
+            _builder.DataSource = db_source;
+            _builder.UserID = db_userid;
+            _builder.Password = db_password;
+            _builder.InitialCatalog = db_name;
+            return new SqlConnection(_builder.ConnectionString);
+        }
+
+        public List<Product> GetProducts()
+        {
+            var products = new List<Product>();
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                var sql = "SELECT * FROM Products";
+                using (var command = new SqlCommand(sql, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            products.Add(new Product
+                            {
+                                ProductId = reader.GetInt32(0),
+                                ProductName = reader.GetString(1),
+                                Quantity = reader.GetInt32(2)
+                            });
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return products;
+        }
+
+
+        //Get Connection string
+        private static string GetConnectionString()
+        {
+            return $"Data Source={db_source};Initial Catalog={db_name};User ID={db_userid};Password={db_password}";
+        }
+
+    }
+}
